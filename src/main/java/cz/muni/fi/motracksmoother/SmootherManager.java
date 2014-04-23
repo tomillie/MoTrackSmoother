266cb642f5f43f2f2838bd88d6f0f11c10f50438
Tomáš Smetanka <tomizzi@tomizzi.com>
2014-04-24 00:28:00 +0200

    Kalman filter commented

--- src/main/java/cz/muni/fi/motracksmoother/SmootherManager.java
+++ src/main/java/cz/muni/fi/motracksmoother/SmootherManager.java
@@ -13,13 +13,18 @@ public interface SmootherManager {
      * Sets some necessary inputs.
      * 
      * @param measurementNoiseCovariance is the Measurement Noise (Error) Covariance given by user
-     * @version 1.0
+     * @version 1.1
      * @since 1.0
      */
     void setInputs(double measurementNoiseCovariance);
 
     /**
      * Prediction phase of Kalman filtering.
+     * <pre>
+     * \hat{x}_{k}^{\scalebox{0.9}{ -}} = A\hat{x}_{k - 1} + Bu_{k - 1} 
+     * 
+     * P_{k}^{\scalebox{0.9}{ -}} = AP_{k - 1}A^{T} + Q 
+     * </pre>
      * 
      * @version 1.0
      * @since 1.0
@@ -29,6 +34,14 @@ public interface SmootherManager {
     /**
      * Correction phase of Kalman filtering.
      * 
+     * <pre>
+     * K_{k} =  P_{k}^{\scalebox{0.9}{ -}}H^{T}(HP_{k}^{\scalebox{0.9}{ -}}H^{T} + R)^{-1}
+     * 
+     * \hat{x}_{k} = x_{k}^{\scalebox{0.9}{ -}} + K_{k}(z_{k} - H\hat{x}_{k}^{\scalebox{0.9}{ -}}) 
+     * 
+     * P_{k} = (1 - K_{k}H)P_{k}^{\scalebox{0.9}{ -}} 
+     * </pre>
+     * 
      * @param z is measured value, which is going to be filtered
      * @return filtered value
      * @version 1.0
@@ -37,7 +50,7 @@ public interface SmootherManager {
     double correct(double z);
 
     /**
-     * Simple transposing of a matrix, which in this case ordinary number.
+     * Simple transpose of a matrix, which in this case is ordinary number.
      * 
      * @param matrix number to be returned
      * @return the same number given as the parameter
--- src/main/java/cz/muni/fi/motracksmoother/SmootherManagerImpl.java
+++ src/main/java/cz/muni/fi/motracksmoother/SmootherManagerImpl.java
@@ -14,43 +14,59 @@ import java.util.TreeMap;
  */
 public class SmootherManagerImpl implements SmootherManager {
 
-    private double priStateEstimate;                    // x^-
+    private double priStateEstimate;                    // \hat{x}^{-}
     private double aMatrix;                             // A
     private double bMatrix;                             // B
     private double hMatrix;                             // H
-    private double predictedMeasurement;                // x^
+    private double predictedMeasurement;                // \hat{x}
     private double actualMeasurement;                   // z
     private double controlInput;                        // u
-    private double priEstErrCovar;                      // P-
+    private double priEstErrCovar;                      // P^{-}
     private double postEstErrCovar;                     // P
     private double measurementNoiseCovar;               // R
     private double processNoiseCovar;                   // Q
     private double kalmanGain;                          // K
 
     public void setInputs(double measurementNoiseCovariance) {
-
+        
+        // following matrices are set to be 1, 
+        // because we implement one dimensional Kalman filter,
+        // which does not operate with matrices
         aMatrix = 1;
         bMatrix = 1;
         hMatrix = 1;
-        predictedMeasurement = 0;
+        
+        // no control input is needed
         controlInput = 0;
-        priEstErrCovar = 1;
+        
+        // if we would have set P to 0, that would mean there was no noise
+        // and all following \hat{x} would be 0
+        // therefore we can use any value different from 0
         postEstErrCovar = 1;
+        
+        // the value is taken from application, set by user
         measurementNoiseCovar = measurementNoiseCovariance;
+        
+        // some very small number, close to 0
         processNoiseCovar = 0.0001;
 
     }
 
     public void setFirstPredictedMeasurement(double firstPosition) {
 
+        // sets initial value of first position to avoid initial noiseness
+        // we can do so, because we know the values befor processing
         predictedMeasurement = firstPosition;
 
     }
 
     public void predict() {
 
+        // first part of \hat{x}^{-}, the prediction
+        // taken from last computed \hat{x}
         priStateEstimate = (aMatrix * predictedMeasurement) + (bMatrix * controlInput);
 
+        // P^{-} computes mostly from P
         priEstErrCovar = (aMatrix * postEstErrCovar * transpose(aMatrix)) + processNoiseCovar;
 
     }
@@ -61,10 +77,14 @@ public class SmootherManagerImpl implements SmootherManager {
 
         actualMeasurement = z;
 
+        // first part of correction, computes Kalman gain
+        // the bigger R is, the smaller K is, therefore K have smaller weight and vice versa
         kalmanGain = (priEstErrCovar * transpose(hMatrix)) / ((hMatrix * priEstErrCovar * transpose(hMatrix)) + measurementNoiseCovar);
 
+        // final correction of the value to be filtered
         predictedMeasurement = priStateEstimate + (kalmanGain * (actualMeasurement - (hMatrix * priStateEstimate)));
 
+        // last part is update of P to be used in next step
         postEstErrCovar = (1 - (kalmanGain * hMatrix)) * priEstErrCovar;
 
         return predictedMeasurement;
@@ -84,6 +104,7 @@ public class SmootherManagerImpl implements SmootherManager {
             TreeMap<JointType, ArrayList<Float>> tempSkeletonPositions = new TreeMap<JointType, ArrayList<Float>>();
             TreeMap<JointType, ArrayList<Float>> tempSkeletonCleanedPositions = new TreeMap<JointType, ArrayList<Float>>();
 
+            // gets positions from Skeleton entity into temporary TreeMap
             switch (i) {
                 case 0:
                     tempSkeletonCleanedPositions.putAll(skeleton.getxPositions());
@@ -98,6 +119,7 @@ public class SmootherManagerImpl implements SmootherManager {
 
             for (Map.Entry entry : tempSkeletonCleanedPositions.entrySet()) {
 
+                // sets initial parameters
                 setInputs(measurementNoiseCovariance);
 
                 ArrayList<Float> tempAxisPositions = new ArrayList<Float>();
@@ -105,12 +127,15 @@ public class SmootherManagerImpl implements SmootherManager {
                 boolean isFirst = true;
 
                 for (Float pos : tempSkeletonCleanedPositions.get((JointType) entry.getKey())) {
-
+                    
+                    // if the measurement is first in its sequence, 
+                    // sets \hat{x} to first measured value
                     if (isFirst) {
                         setFirstPredictedMeasurement(pos);
                         isFirst = false;
                     }
 
+                    // applies Kalman filter
                     pos = (float) correct(pos);
 
                     tempAxisPositions.add(pos);
@@ -120,6 +145,7 @@ public class SmootherManagerImpl implements SmootherManager {
 
             }
 
+            // sets cleaned positions of Skeleton entity
             switch (i) {
                 case 0:
                     skeleton.setxPositionsCleaned(tempSkeletonPositions);
